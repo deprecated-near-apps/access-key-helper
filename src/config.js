3724d0633c24244da9cdfdb14cc9847799fbd6d3
Josh <thisisjoshford@gmail.com>
2021-05-17 16:24:29 -0700

    update config

--- src/config.js
+++ src/config.js
@@ -1,35 +1,35 @@
-const contractName = 'dev-1618863126771-1895832';
+const CONTRACT_NAME = "dev-1618863126771-1895832";
 
 module.exports = function getConfig(isServer = false) {
-	let config = {
-		networkId: 'default',
-		nodeUrl: 'https://rpc.testnet.near.org',
-		walletUrl: 'https://wallet.testnet.near.org',
-		helperUrl: 'https://helper.testnet.near.org',
-		contractName,
-	};
-    
-	if (process.env.REACT_APP_ENV !== undefined) {
-		config = {
-			...config,
-			GAS: '200000000000000',
-			DEFAULT_NEW_ACCOUNT_AMOUNT: '5',
-			contractMethods: {
-				changeMethods: ['add_access_key', 'delete_access_key'],
-			},
-		};
-	}
-    
-	if (process.env.REACT_APP_ENV === 'prod') {
-		config = {
-			...config,
-			networkId: 'mainnet',
-			nodeUrl: 'https://rpc.mainnet.near.org',
-			walletUrl: 'https://wallet.near.org',
-			helperUrl: 'https://helper.mainnet.near.org',
-			contractName: 'near',
-		};
-	}
+  let config = {
+    networkId: "testnet",
+    nodeUrl: "https://rpc.testnet.near.org",
+    walletUrl: "https://wallet.testnet.near.org",
+    helperUrl: "https://helper.testnet.near.org",
+    CONTRACT_NAME,
+  };
 
-	return config;
+  if (process.env.REACT_APP_ENV !== undefined) {
+    config = {
+      ...config,
+      GAS: "200000000000000",
+      DEFAULT_NEW_ACCOUNT_AMOUNT: "5",
+      contractMethods: {
+        changeMethods: ["add_access_key", "delete_access_key"],
+      },
+    };
+  }
+
+  if (process.env.REACT_APP_ENV === "prod") {
+    config = {
+      ...config,
+      networkId: "mainnet",
+      nodeUrl: "https://rpc.mainnet.near.org",
+      walletUrl: "https://wallet.near.org",
+      helperUrl: "https://helper.mainnet.near.org",
+      CONTRACT_NAME: "near",
+    };
+  }
+
+  return config;
 };
